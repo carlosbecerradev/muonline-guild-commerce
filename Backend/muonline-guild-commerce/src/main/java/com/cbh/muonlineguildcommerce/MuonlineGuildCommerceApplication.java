3479fbbcaa9a0737f751aef7bc25ac2bdd33bd60
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-01-30 19:12:30 -0500

    :tada: backend project.

---
+++ Backend/muonline-guild-commerce/src/main/java/com/cbh/muonlineguildcommerce/MuonlineGuildCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.cbh.muonlineguildcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuonlineGuildCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuonlineGuildCommerceApplication.class, args);
+	}
+
+}
---
+++ Backend/muonline-guild-commerce/src/test/java/com/cbh/muonlineguildcommerce/MuonlineGuildCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbh.muonlineguildcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuonlineGuildCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
