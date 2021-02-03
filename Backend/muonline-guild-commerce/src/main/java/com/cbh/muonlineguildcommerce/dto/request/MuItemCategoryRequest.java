0002a5d4ed88119bd71e6773b462c505eae19fda
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-02-03 17:01:03 -0500

    :fire: Remove code to trim Whitespace in front of name value.

--- Backend/muonline-guild-commerce/src/main/java/com/cbh/muonlineguildcommerce/dto/request/MuItemCategoryRequest.java
+++ Backend/muonline-guild-commerce/src/main/java/com/cbh/muonlineguildcommerce/dto/request/MuItemCategoryRequest.java
@@ -1,7 +1,6 @@
 package com.cbh.muonlineguildcommerce.dto.request;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
@@ -13,11 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MuItemCategoryRequest {
 	@NotBlank
-	@NotNull(message = "Name should not be empty")
 	@Size(min = 1, max = 60, message = "Name should be between 1 and 60 characters")
 	private String name;
-
-	public void setName(String name) {
-		this.name = name.trim();
-	}
 }
