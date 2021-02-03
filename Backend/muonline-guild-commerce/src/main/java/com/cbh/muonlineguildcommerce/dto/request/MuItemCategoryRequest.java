31517a18064096fd7454acdf6dd5d84a5b2e7090
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-02-03 15:36:35 -0500

    :sparkles: Add whitespaces validation.

--- Backend/muonline-guild-commerce/src/main/java/com/cbh/muonlineguildcommerce/dto/request/MuItemCategoryRequest.java
+++ Backend/muonline-guild-commerce/src/main/java/com/cbh/muonlineguildcommerce/dto/request/MuItemCategoryRequest.java
@@ -1,5 +1,6 @@
 package com.cbh.muonlineguildcommerce.dto.request;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,7 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MuItemCategoryRequest {
+	@NotBlank
 	@NotNull(message = "Name should not be empty")
 	@Size(min = 1, max = 60, message = "Name should be between 1 and 60 characters")
 	private String name;
+	
+	public void setName(String name) {
+		this.name = name.trim();
+	}
 }
