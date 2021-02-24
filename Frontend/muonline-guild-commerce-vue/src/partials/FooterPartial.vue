0809cc66f7665496524ba7277a996e33ecb90500
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-02-24 17:00:08 -0500

    :sparkles: Add Partials.

---
+++ Frontend/muonline-guild-commerce-vue/src/partials/FooterPartial.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="bg-gray-100 border-t border-gray-300 mt-auto">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-2">
+      <div class="text-gray-900 text-center">
+        <p>Designed By carlosbecerradev</p>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ Frontend/muonline-guild-commerce-vue/src/partials/HeaderPartial.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <main-navigation />
+  </header>
+</template>
+
+<script>
+import MainNavigation from "@/components/nav/MainNavigation.vue";
+
+export default {
+  components: {
+    MainNavigation,
+  },
+};
+</script>
