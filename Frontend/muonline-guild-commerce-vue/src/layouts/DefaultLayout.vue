b70a4bbb224e2f1e6182d684f61b6f302bd9917c
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-02-24 17:00:38 -0500

    :sparkles: Add Layouts.

---
+++ Frontend/muonline-guild-commerce-vue/src/layouts/DefaultLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Default Layout -->
+  <div class="min-h-screen flex flex-col">
+    <header-partial />
+    <main class="main">
+      <slot />
+    </main>
+    <footer-partial />
+  </div>
+</template>
+
+<script>
+import HeaderPartial from "@/partials/HeaderPartial.vue";
+import FooterPartial from "@/partials/FooterPartial.vue";
+
+export default {
+  components: {
+    HeaderPartial,
+    FooterPartial,
+  },
+};
+</script>
---
+++ Frontend/muonline-guild-commerce-vue/src/layouts/PageLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="min-h-screen bg-gray-900">
+    <header-partial />
+    <div class="flex items-center justify-center py-12 sm:px-6 lg:px-8">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderPartial from "@/partials/HeaderPartial.vue";
+
+export default {
+  components: {
+    HeaderPartial,
+  },
+};
+</script>
