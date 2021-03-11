4131bd4e2187d677ff2dd3f50b7a70ea13e191e8
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-03-11 12:19:43 -0500

    :sparkles: Add CancelButton component.

---
+++ Frontend/muonline-guild-commerce-vue/src/components/button/CancelButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    @click="cancelOperation"
+    class="flex w-full justify-center py-2 px-4 shadow font-medium rounded-md text-gray-900 border border-transparent hover:border-gray-900 cursor-pointer"
+  >
+    Cancel
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    cancelOperation() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
